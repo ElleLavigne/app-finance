cb7ce7dd6e22ed7d6d043a82b7b5cb7b620f5a09
Izaelle <izaelleavrillavigne@gmail.com>
2025-01-16 14:04:40 -0300

    feat: filter home page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,27 +8,26 @@ import { IncomeSummaryCard } from "@/components/incomeSummaryCard";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Calendar } from "@/components/ui/calendar";
+
+import { DatePickerWithRange } from "@/components/date-picker-with-range";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col pl-10 pr-10 gap-11">
-        <div className="flex justify-between">
+        <div className="flex ">
           <p className="text-4xl text-foreground font-semibold">Resumo Geral</p>
+          
           <DropdownMenu>
-            <DropdownMenuTrigger className="">
+            <DropdownMenuTrigger className="py-2 px-4 border rounded-xl ml-auto">
               Filtro por data
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className="">
               {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
               {/* <DropdownMenuSeparator /> */}
-              <Calendar />
+              <DatePickerWithRange/>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
